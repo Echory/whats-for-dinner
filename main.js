09690f349894555575d3276bfaf6015aff0801c2
Emma Chory <emmachory@gmail.com>
2021-10-27 11:37:15 -0600

    Create side, main, dessert variables

---
+++ main.js
@@ -0,0 +1,46 @@
+var sides = [
+'Miso Glazed Carrots',
+'Coleslaw',
+'Garden Salad',
+'Crispy Potatoes',
+'Sweet Potato Tots',
+'Coconut Rice',
+'Mashed Potatoes',
+'Summer Rolls',
+'Garlic Mushrooms',
+'Broccoli'
+];
+
+var mains = [
+'Vegetable Lasagna',
+'Pineapple Fried Rice',
+'Shakshuka',
+'Thai Yellow Curry',
+'Bibimbap',
+'3 Bean Chili',
+'Butternut Squash Soup',
+'Chickpea Noodle Soup',
+'Ramen',
+'Empanadas',
+'Pesto Pasta',
+'Sheet Pan Fajitas',
+'Margarita Pizza'
+];
+
+var desserts = [
+'Apple Pie',
+'Lemon Meringue Pie',
+'Black Forest Cake',
+'Banana Bread',
+'Peach Cobbler',
+'Funfetti Cake',
+'Baklava',
+'Apple Crumble',
+'Macaroons',
+'Chocolate Cupcakes',
+'Pavlova',
+'Pumpkin Pie',
+'Key Lime Pie',
+'Chocolate Croissants',
+'Eclairs'
+];
